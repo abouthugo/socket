87d8d1608bd82aa6db93afc975ab62aa0306fbf8
Hugo Perdomo <hugoperdomo07@gmail.com>
2018-04-04 20:10:21 -0400

    TCP examples copied and working as expected

--- TCPClient.py
+++ TCPClient.py
@@ -1,31 +1,29 @@
-import socket
-import sys
+from socket import *
 
-HOST = '192.168.1.5'
-PORT = 12345
-BUFSIZ = 256
-ADDR = (HOST, PORT)
+DEFAULT_IP = "localhost"  # The server's IP address, change when accessed remotely
+DEFAULT_PORT = 12345  # Arbitrary port number
+BUFFER_SIZE = 4096
+DEFAULT_ADDRESS = (DEFAULT_IP, DEFAULT_PORT)  # For readability purposes
+TRANSPORT_PROTOCOL = SOCK_STREAM  # The TCP protocol
+IP_FAM = AF_INET  # IPv4 family
 
 if __name__ == '__main__':
-       client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       try:
-        client_sock.connect(ADDR)
-       except socket.error as error:
-           print(f"Socket not created: {error}")
-           sys.exit()
-
-       payload = 'GET TIME'
-       try:
-           while True:
-               client_sock.send(payload.encode('utf-8'))
-               data = client_sock.recv(BUFSIZ)
-               print(repr(data))
-               more = input("Want to send more data to the server? [y/n]: ")
-               if more.lower() == 'y':
-                   payload = input("Enter payload: ")
-               else:
-                   break
-       except KeyboardInterrupt:
-           print("Exited by user")
-
-       client_sock.close()
+    client = socket(IP_FAM, TRANSPORT_PROTOCOL)  # Create socket
+    try:
+        client.connect(DEFAULT_ADDRESS)  # Connect to server
+    except ConnectionRefusedError as e:
+        print('Connection refused')
+    payload = 'GET TIME'
+    try:
+        while True:
+            client.send(payload.encode('utf-8'))  # send data encoded
+            data = client.recv(BUFFER_SIZE)  # receive response from server
+            print(repr(data))  # print the string representation of the time object
+            more = input("Want to send more data to the server? [y/n]: ")  # ask user for more data
+            if more.lower() == 'y':
+                payload = input("Enter payload: ")
+            else:
+                break
+    except KeyboardInterrupt:
+        print("Exited by user")
+    client.close()  # close connection
--- TCPServer.py
+++ TCPServer.py
@@ -1,34 +1,36 @@
 from socket import *
 from time import ctime
 
-IP = "192.168.1.8"  # The server's IP address
-PORT = 12365  # Arbitrary port number
-BUFFER_SIZE = 1024  
+IP = 'localhost'  # The server's IP address
+PORT = 12345  # Arbitrary port number
+BUFFER_SIZE = 4096
 ADDRESS = (IP, PORT)  # For readability purposes
 TRANSPORT_PROTOCOL = SOCK_STREAM  # The TCP protocol
 IP_FAM = AF_INET  # IPv4 family
 
 
 if __name__ == '__main__':
-    server_socket = socket(IP_FAM, TRANSPORT_PROTOCOL)
-    server_socket.bind(ADDRESS)  # Bind to address
-    server_socket.listen(5)  # Accepts 5 connections
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # SO_REUSE ADDR tells the kernel to reuse a local socket, without waiting for its natural timeout to expire
+    server = socket(IP_FAM, TRANSPORT_PROTOCOL)
+    server.bind(ADDRESS)  # Bind to address
+    server.listen(5)  # Accepts 5 connections
+    # SOL_SOCKET is the level argument
+    # SO_REUSEADDR,1 means permit use of local addresses for this socket
+    server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
     while True:
-        print("Server waiting for connection...")
-        client_sock, addr = server_socket.accept()
-
-        while True:
-            data = client_sock.recv(BUFFER_SIZE)
-            if not data or data.decode('utf-8') == 'END':
-                break
-            print(f"Received from client {data.decode('utf-8')}")
-            print(f'Sending the server time to client: {ctime()}')
-            try:
+        try:
+            print("Server waiting for connection...")
+            client_sock, addr = server.accept()  # accept a connection, receives a socket object and an address for it
+            print(f"Client connected from {addr}")
+            while True:
+                data = client_sock.recv(BUFFER_SIZE)  # prepare to receive
+                if not data or data.decode('utf-8') == 'END':
+                    break
+                print(f"Received from client {data.decode('utf-8')}")
+                print(f'Sending the server time to client: {ctime()}')
                 client_sock.send(bytes(ctime(), 'utf-8'))
-            except KeyboardInterrupt:
-                print("Exited by user")
+        except KeyboardInterrupt:
+            print("\nExited by user")
+            break
         client_sock.close()
-    server_socket.close()
+    server.close()
